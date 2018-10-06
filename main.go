7998db9176e33e258b1bd5b9d0fcdf8ec852a8a4
lars <lars.scheme@gmail.com>
2018-10-06 12:37:00 +0200

    Complete the C port

--- main.go
+++ main.go
@@ -24,60 +24,23 @@ func runMain() int {
 
 	fd, err := os.OpenFile(filename, os.O_WRONLY, 0)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		fmt.Fprintln(os.Stderr, "Error opening output file:", err)
 		return 1
 	}
 	defer fd.Close()
 
 	fmt.Println("Resetting USB device", filename)
-	r1, r2, errno := syscall.Syscall(
+	ret, _, err := syscall.Syscall(
 		syscall.SYS_IOCTL,
 		fd.Fd(),
 		USBDEVFS_RESET,
 		0,
 	)
-	fmt.Println(r1, r2, errno)
+	if ret != 0 {
+		fmt.Fprintln(os.Stderr, "Error in ioctl:", err)
+		return 1
+	}
+	fmt.Println("Reset successful")
 
 	return 0
 }
-
-/*
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/ioctl.h>
-
-#include <linux/usbdevice_fs.h>
-
-
-int main(int argc, char **argv)
-{
-	const char *filename;
-	int fd;
-	int rc;
-
-	if (argc != 2) {
-		fprintf(stderr, "Usage: usbreset device-filename\n");
-		return 1;
-	}
-	filename = argv[1];
-
-	fd = open(filename, O_WRONLY);
-	if (fd < 0) {
-		perror("Error opening output file");
-		return 1;
-	}
-
-	printf("Resetting USB device %s\n", filename);
-	rc = ioctl(fd, USBDEVFS_RESET, 0);
-	if (rc < 0) {
-		perror("Error in ioctl");
-		return 1;
-	}
-	printf("Reset successful\n");
-
-	close(fd);
-	return 0;
-}
-*/
